ce96bb58639b68ef2b1b28052173f280521e1917
Matthew Volpe <you@example.com>
2020-06-03 23:37:27 -0700

    HW 4 step 1

---
+++ hw4/wchild.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    // For each argument exluding argv[0] (The program's name)...
+    for (short ii = 1; ii < argc; ++ii)
+    {
+        // Print the argument to STDOUT (fd1) and a space.
+        write(1, argv[ii], strlen(argv[ii]));
+        write(1, " ", 1);
+    }
+
+    return 0;
+}
