11bc7701cefd342d93e0aa0346906f0d27247e66
Matthew Volpe <you@example.com>
2020-06-13 21:20:05 -0700

    Hw 4 turn in but for real this time.

--- hw4/pecho.c
+++ hw4/pecho.c
@@ -14,9 +14,11 @@ int main(int argc, char * argv[])
 {
     int pipefd[2];
 
+    // Open the pipe.
     if (pipe(pipefd) == -1)
         fatal_error("Unable to open pipe.");
 
+    // Create the fork for the wchild process.
     pid_t wproc = fork();
 
     if (wproc == -1)
@@ -25,26 +27,33 @@ int main(int argc, char * argv[])
     // wproc process
     else if (wproc == 0)
     {
+        // Close the input pipe.
         if (close(pipefd[0]))
             fatal_error("Failed to close pipefd[0] in wproc.");
 
+        // Change argv to be used to run wchild instead.
         char command[] = "./wchild";
         argv[0] = command;
 
+        // Call wchild.
         if (execvp(argv[0], argv))
             fatal_error("Unable to execute wchild.");
 
+        // Close the pipe.
         if (close(pipefd[1]))
             fatal_error("Failed to close pipefd[1] in wproc.");
 
+        // Close the process.
         exit(0);
     }
 
     // Parent process
     else
     {
+        // Wait for wchild to finish.
         wait(NULL);
 
+        // Create the rchild process.
         pid_t rproc = fork();
         if (rproc == -1)
             fatal_error("Unable to fork rproc.");
@@ -52,21 +61,25 @@ int main(int argc, char * argv[])
         // rproc process
         if (rproc == 0)
         {
-            wait(NULL);
-
+            // Close the output pipe.
             if (close(pipefd[1]))
                 fatal_error("Failed to close pipefd[1] in rproc.");
 
+            // Prep to use execvp for rchild with no arguments, and use it.
             char * args[] = {"./rchild",NULL};
             if (execvp(args[0], args))
                 fatal_error("Unable to execute rchild.");
 
+            // Close the pipe.
             if (close(pipefd[0]))
                 fatal_error("Unable to close input pipefd[0] in rproc.");
 
+            // Close the process.
             exit(0);
         }
     }
+
+    // I thought I shoudl do pclose here, but it gives a segmentation fault, which I'm guessing is because I close the pipe on both sides in other places?
 
     return 0;
 }
